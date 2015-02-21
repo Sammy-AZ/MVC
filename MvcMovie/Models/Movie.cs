4bedc36c972da27053a0df064b2833ce26da7ed0
Samy_Hammami <samy.Hammami@hotmail.com>
2015-02-21 15:54:57 +0000

    Chapter 6 : Accessing Your Model's Data from a Controller

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace MvcMovie.Models
@@ -7,6 +8,8 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
